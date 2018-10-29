ede1a461a5670e0a00f87eb082a6d9726801abe3
AST-Isabella <43156348+AST-Isabella@users.noreply.github.com>
2018-10-29 16:21:30 -0500

    repaired compiler error
    Main Activity was missing so I added it again and changed a couple other things slightly

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/coordinatorlayout/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/coordinatorlayout/R.java
@@ -87,41 +87,41 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int action_container = 0x7f080016;
-        public static final int action_divider = 0x7f080018;
-        public static final int action_image = 0x7f080019;
-        public static final int action_text = 0x7f08001f;
-        public static final int actions = 0x7f080020;
-        public static final int async = 0x7f080026;
-        public static final int blocking = 0x7f080029;
-        public static final int bottom = 0x7f08002a;
-        public static final int chronometer = 0x7f080032;
-        public static final int end = 0x7f080043;
-        public static final int forever = 0x7f08004a;
-        public static final int icon = 0x7f080050;
-        public static final int icon_group = 0x7f080051;
-        public static final int info = 0x7f080054;
-        public static final int italic = 0x7f080056;
-        public static final int left = 0x7f080058;
-        public static final int line1 = 0x7f080059;
-        public static final int line3 = 0x7f08005a;
-        public static final int none = 0x7f080065;
-        public static final int normal = 0x7f080066;
-        public static final int notification_background = 0x7f080067;
-        public static final int notification_main_column = 0x7f080068;
-        public static final int notification_main_column_container = 0x7f080069;
-        public static final int right = 0x7f080073;
-        public static final int right_icon = 0x7f080074;
-        public static final int right_side = 0x7f080075;
-        public static final int start = 0x7f080091;
-        public static final int tag_transition_group = 0x7f080096;
-        public static final int tag_unhandled_key_event_manager = 0x7f080097;
-        public static final int tag_unhandled_key_listeners = 0x7f080098;
-        public static final int text = 0x7f080099;
-        public static final int text2 = 0x7f08009a;
-        public static final int time = 0x7f08009f;
-        public static final int title = 0x7f0800a0;
-        public static final int top = 0x7f0800a3;
+        public static final int action_container = 0x7f080015;
+        public static final int action_divider = 0x7f080017;
+        public static final int action_image = 0x7f080018;
+        public static final int action_text = 0x7f08001e;
+        public static final int actions = 0x7f08001f;
+        public static final int async = 0x7f080025;
+        public static final int blocking = 0x7f080028;
+        public static final int bottom = 0x7f080029;
+        public static final int chronometer = 0x7f080031;
+        public static final int end = 0x7f080042;
+        public static final int forever = 0x7f080049;
+        public static final int icon = 0x7f08004f;
+        public static final int icon_group = 0x7f080050;
+        public static final int info = 0x7f080053;
+        public static final int italic = 0x7f080055;
+        public static final int left = 0x7f080057;
+        public static final int line1 = 0x7f080058;
+        public static final int line3 = 0x7f080059;
+        public static final int none = 0x7f080064;
+        public static final int normal = 0x7f080065;
+        public static final int notification_background = 0x7f080066;
+        public static final int notification_main_column = 0x7f080067;
+        public static final int notification_main_column_container = 0x7f080068;
+        public static final int right = 0x7f080072;
+        public static final int right_icon = 0x7f080073;
+        public static final int right_side = 0x7f080074;
+        public static final int start = 0x7f080090;
+        public static final int tag_transition_group = 0x7f080095;
+        public static final int tag_unhandled_key_event_manager = 0x7f080096;
+        public static final int tag_unhandled_key_listeners = 0x7f080097;
+        public static final int text = 0x7f080098;
+        public static final int text2 = 0x7f080099;
+        public static final int time = 0x7f08009e;
+        public static final int title = 0x7f08009f;
+        public static final int top = 0x7f0800a2;
     }
     public static final class integer {
         private integer() {}
